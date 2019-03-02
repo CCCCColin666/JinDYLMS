5046e2174575aaecd0a5463479aff605e7b02c32
CCCCColin666 <baizccolin@outlook.com>
2019-03-02 16:37:52 +0800

    colin 3-2 commit

--- WebContent/js/finance/finance.js
+++ WebContent/js/finance/finance.js
@@ -1,20 +1,40 @@
 $(function(){
 	$(".option").click(function(){
-		$(this).next(".option_child").toggle();
-		$(this).next(".option_child").next(".option_child").toggle();
+		$(this).next(".option_child").slideToggle("fast");
+		$(this).next(".option_child").next(".option_child").slideToggle("fast");
 	})
 	
 	
+	$(".option_child:eq(0)").click(function(){
+		$("#div2_function1").toggle();
+		$("#div2_function2").hide();
+	})
+	
+	$(".option_child:eq(1)").click(function(){
+		$("#div2_function2").toggle();
+		$("#div2_function1").hide();
+	})
+	
+	
+	$("#select>input").focus(function(){
+		$("#tip1").show();
+	})
+	
+	$("#select>input").blur(function(){
+		$("#tip1").hide();
+	})
+	
 	$(".option").click(function(){
-		alert(123);
 		$.ajax({
 			url:"../../selectInvoiceInfo",
 			type:"post",
 			success:function(data){
-				alert(data);
+				
 			}
 		});
-	
 	});
+	
+	
+	
 	
 });
