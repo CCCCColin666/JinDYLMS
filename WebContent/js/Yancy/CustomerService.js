6a33a9deda2e5d8ad0364f34c05c4492721ce98b
Yancy <Yancy@192.168.1.101>
2019-03-04 02:40:45 +0800

    Yancy

---
+++ WebContent/js/Yancy/CustomerService.js
@@ -0,0 +1,26 @@
+
+$(function(){
+	$('#dg').datagrid({    
+    url:'datagrid_data1.json',    
+    columns:[[    
+        {field:'productid',title:'商品编号',width:100},    
+        {field:'productname',title:'商品名称',width:100},    
+        {field:'unitcost',title:'商品价格',width:100,align:'right'},  
+        {field:'status',title:'商品状态',width:100,align:'right'},
+        {field:'attr1',title:'商品属性',width:100,align:'right'},
+        {field:'xxxxx',title:'操作',width:100,align:'left',formatter: function(value,row,index){
+					return "<a href=''>修改</a>|<a href=''>删除</a>";
+			}
+        }
+    ]],
+    striped:true,
+    //显示分页工具栏
+    pagination:true,
+    pageSize:5,
+    pageList:[5,10,15],
+    
+	});  
+
+	
+});
+
